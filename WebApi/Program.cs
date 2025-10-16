0bfc99f4b8630181d0564cbec3bc5269d1742dab
kubrastack <kmrt3014@gmail.com>
2025-10-16 18:19:35 +0300

    JWT kimlik doğrulama ve CORS yapılandırması eklendi.
    - `Program.cs` dosyasına JWT tabanlı kimlik doğrulama eklendi.
    - CORS yapılandırması ile `http://localhost:4200` adresine izin verildi.
    - `TokenOptions` yapılandırması tanımlandı ve servisler eklendi.
    - Swagger/OpenAPI desteği sağlandı.
    - `WebApi.csproj` dosyasına `Application` ve `Persistence` projelerine referanslar eklendi.
    - `Controllers` klasörü projeye dahil edildi.

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,6 +1,37 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigins",
+         builder =>
+         {
+             builder.WithOrigins("http://localhost:4200")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowCredentials();
+         });
+});
 
 // Add services to the container.
+
+TokenOptions? tokenOptions = builder.Configuration.GetSection("TokenOptions").Get<TokenOptions>();
+builder.Services.AddCoreServices(tokenOptions!);
+
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(
+        options =>
+        {
+            options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+            {
+                ValidateIssuer = true,
+
+            };
+
+        }
+    );
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
